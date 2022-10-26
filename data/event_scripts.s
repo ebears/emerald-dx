0fbd8291a9bb3a0ce19067bb1c2316b72776dbc7
ebears <thomlbmclean@gmail.com>
2022-10-26 02:51:55 -0400

    Littleroot Connection

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1061,3 +1061,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/LittlerootConnection/scripts.inc"
